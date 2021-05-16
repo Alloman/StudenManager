4857bcd6cea339de3e5f34fe1d9fb3c1bb7a56af
Alloman <491800106@qq.com>
2021-05-16 15:15:21 +0800

    add model class

--- exp2/administrators/models.py
+++ exp2/administrators/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Administrators(models.Model):
+    sid = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
--- exp2/teacher/models.py
+++ exp2/teacher/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Teacher(models.Model):
+   sid = models.CharField(max_length=50)
+   name = models.CharField(max_length=50)
+   phone = models.CharField(max_length=50)
