ac524f04d1e75712b16a87921ed4dcf51b30830b
Alloman <491800106@qq.com>
2021-05-16 11:06:35 +0800

    add mysql

--- exp2/exp2/settings.py
+++ exp2/exp2/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'exp2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'exp2',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
